eaa9e2ad7f8e65ec6ba3beca4a622b1b616388b4
FIRST_NAME LAST_NAME <hassan.baig1228@gmail.com>
2023-08-10 20:41:50 +0500

    added all file

---
+++ src/pages/pricing.js
@@ -0,0 +1,36 @@
+import PageBanner from "@/components/BannerSection/PageBanner";
+import CallToSection from "@/components/CallToSection/CallToSection";
+
+import HeaderTwo from "@/components/Header/HeaderTwo";
+import MobileMenu from "@/components/Header/MobileMenu";
+import Layout from "@/components/Layout/Layout";
+import FooterSeven from "@/components/MainFooter/FooterSeven";
+import PricingOne from "@/components/PricingSection/PricingOne";
+import Style from "@/components/Reuseable/Style";
+import SearchPopup from "@/components/SearchPopup/SearchPopup";
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <Layout pageTitle="Pricing Page">
+      <Style
+            font="Rubik, sans-serif"
+            bFont="Rubik, sans-serif"
+            black="#2a2833"
+            text="#74727a"
+            base="#ff4b82"
+            baseRgb="255, 75, 130"
+            scrollToTopColor="#fff"
+            blackRgb="42, 40, 51"  />
+      <HeaderTwo  headerStyle="mainmenu-seven" header={7} />
+      <MobileMenu />
+      <SearchPopup />
+      <PageBanner title="Pricing Plans" page="Pricing Page" parent="Page" />
+      <PricingOne />
+      <CallToSection />
+      <FooterSeven  normalPadding={false} />
+    </Layout>
+  );
+};
+
+export default Pricing;
