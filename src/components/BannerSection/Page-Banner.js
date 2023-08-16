f038be35a8e0289b904583a6a9a9659381539e0c
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2023-08-16 19:59:22 +0500

    Add files via upload

---
+++ src/components/BannerSection/Page-Banner.js
@@ -0,0 +1,44 @@
+
+
+import Link from "next/link";
+import React from "react";
+
+const PageBanner = ({
+  title = "",
+  page = "",
+  parent = "",
+  parentHref = "/",
+}) => {
+  return (
+    <section className="page-banner">
+      <div
+        className="image-layer"
+      
+      ></div>
+      <div className="shape-1"></div>
+      <div className="shape-2"></div>
+      <div className="banner-inner">
+        <div className="auto-container">
+          <div className="inner-container clearfix">
+            <h1>{title}</h1>
+            <div className="page-nav">
+              <ul className="bread-crumb clearfix">
+                <li>
+                  <Link href="/">Home</Link>
+                </li>
+                {parent && (
+                  <li>
+                    <Link href={parentHref}>{parent}</Link>
+                  </li>
+                )}
+                <li className="active">{page || title}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PageBanner;
