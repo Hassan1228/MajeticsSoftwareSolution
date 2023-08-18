42276390f92bd7966b331cf8bbe7a749ecf8c982
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2023-08-18 20:55:03 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+
 import AboutNine from "@/components/AboutSection/AboutNine";
 import FunFactSeven from "@/components/FunFacts/FunFactSeven";
 import HeaderTwo from "@/components/Header/HeaderTwo";
@@ -24,21 +24,7 @@ import Text from "@/components/Text";
 
 const Home7 = () => {
   return (
-    <>
-    <Head>
-        <link rel="stylesheet" href="/style.css" />
-        <link rel="stylesheet" href="https://unpkg.com/kursor/dist/kursor.css" />
-        <script src="https://unpkg.com/kursor"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              new kursor({
-                type: 1
-              });
-            `,
-          }}
-        ></script>
-      </Head>
+ 
     <Layout pageTitle="Home " preloader={preloader}>
       <Style
         font="Rubik, sans-serif"
@@ -69,13 +55,13 @@ const Home7 = () => {
       <SubscribeTwo />
       <FooterSeven />
     </Layout>
-          </>
+         
   );
 };
 
 export default Home7;
 
-// pages/index.js
+
 
 
 
