9d6081ba42c1cb30abdce78c375665adb8c217eb
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2023-08-18 19:56:20 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,20 +15,11 @@ import "tiny-slider/dist/tiny-slider.css";
 import "@/styles/style.css";
 import "@/styles/hover.css";
 import "@/styles/responsive.css";
-import Script from 'next/script';
+
 const MyApp = ({ Component, pageProps }) => {
   
   return (
-     <Head>
-        <title>First Post</title>
-      </Head>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
+   
     <ContextProvider>
       <Component {...pageProps} />
     </ContextProvider>
