e686c464d0cc8f0ce84f66308065f68038379fdc
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2023-08-18 20:58:45 +0500

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
-
+import Head from 'next/head';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
