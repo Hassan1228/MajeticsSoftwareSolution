d904975d9b2969f1d1a804ea8d3a095ced2cc98f
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2023-08-18 20:27:57 +0500

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -38,10 +37,7 @@ class MyDocument extends Document {
             type="image/x-icon"
           />
           <link rel="icon" href="favicon.ico" />
-              <link rel="stylesheet" href="kursor.css" />
-             <script src="kursor.js"></script>
-               <script src="Custom.js"></script>
-
+       
               
               <meta
             name="description"
