f63ced5ca04b0a614614a0005ffddc39e62737bc
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2023-08-22 22:27:41 +0500

    Update CustomCursor.js

--- src/components/CustomCursor.js
+++ src/components/CustomCursor.js
@@ -17,7 +17,7 @@ const CustomCursor = () => {
   }, []);
 
   return (
-    <div className={styles.container}>
+ 
       <div
         className={styles.cursor}
         style={{
@@ -25,7 +25,7 @@ const CustomCursor = () => {
           top: `${cursorPosition.y}px`,
         }}
       ></div>
-    </div>
+
   );
 };
 
