e6f0aa80eba9499c5049b6e56df2304d9960aea8
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2023-08-22 22:20:02 +0500

    Create CustomCursor.js

---
+++ src/components/CustomCursor.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/CustomCursor.module.css';
+
+const CustomCursor = () => {
+  const [cursorPosition, setCursorPosition] = useState({ x: -100, y: -100 });
+
+  const updateCursorPosition = (e) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', updateCursorPosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateCursorPosition);
+    };
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.cursor}
+        style={{
+          left: `${cursorPosition.x}px`,
+          top: `${cursorPosition.y}px`,
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default CustomCursor;
