4b55417dafd6ef69f96d7227225e266e80b4252a
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2023-08-25 00:13:29 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,9 @@
-
-import AboutNine from "@/components/AboutSection/AboutNine";
-import FunFactSeven from "@/components/FunFacts/FunFactSeven";
-import HeaderTwo from "@/components/Header/HeaderTwo";
-import MobileMenu from "@/components/Header/MobileMenu";
-import Layout from "@/components/Layout/Layout";
-import FooterSeven from "@/components/MainFooter/FooterSeven";
-import BlogSeven from "@/components/NewsSection/BlogSeven";
-
-import ProjectSeven from "@/components/ProjectSection/ProjectSeven";
-import Style from "@/components/Reuseable/Style";
-import SearchPopup from "@/components/SearchPopup/SearchPopup";
-
-import SliderSeven from "@/components/SliderSection/SliderSeven";
-import SponsorSeven from "@/components/SponsorsSection/SponsorSeven";
-import SubscribeTwo from "@/components/SubscribeSection/SubscribeTwo";
-
-import preloader from "@/images/icons/preloader.png";
 import React from "react";
-import Text from "@/components/Text";
-import Text from "@/components/Home";
-
-
-
+import Home from "@/components/Home";
 
 
 const Home7 = () => {
   return (
- 
  
  <>
     <Home/>
