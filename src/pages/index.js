e388629babba43fa01b420c1cabc0b25640fa605
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2023-08-25 00:14:50 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,60 @@
-import React from "react";
-import Home from "@/components/Home";
+import AboutEight from "@/components/AboutSection/AboutEight";
+import AboutNine from "@/components/AboutSection/AboutNine";
+import FunFactSeven from "@/components/FunFacts/FunFactSeven";
+import HeaderTwo from "@/components/Header/HeaderTwo";
+import MobileMenu from "@/components/Header/MobileMenu";
+import Layout from "@/components/Layout/Layout";
+import FooterSeven from "@/components/MainFooter/FooterSeven";
+import BlogSeven from "@/components/NewsSection/BlogSeven";
+import PricingTwo from "@/components/PricingSection/PricingTwo";
+import ProjectSeven from "@/components/ProjectSection/ProjectSeven";
+import Style from "@/components/Reuseable/Style";
+import SearchPopup from "@/components/SearchPopup/SearchPopup";
+import ServiceSeven from "@/components/ServicesSection/ServiceSeven";
+import SliderSeven from "@/components/SliderSection/SliderSeven";
+import SponsorSeven from "@/components/SponsorsSection/SponsorSeven";
+import SubscribeTwo from "@/components/SubscribeSection/SubscribeTwo";
 
+import preloader from "@/images/icons/preloader.png";
+import React from "react";
+import Text from "@/components/Text";
+import TestimonialsSection from "@/components/TestimonialsSection/TestimonialsSection";
+import WeDOSection from "@/components/WeDOSection/WeDOSection";
+import AboutSection from "@/components/AboutSection/AboutSection";
 
 const Home7 = () => {
   return (
- 
- <>
-    <Home/>
-    </>
-         
+    <Layout pageTitle="Home " preloader={preloader}>
+      <Style
+        font="Rubik, sans-serif"
+        bFont="Rubik, sans-serif"
+        black="#2a2833"
+        text="#74727a"
+        base="#ff4b82"
+        baseRgb="255, 75, 130"
+        scrollToTopColor="#fff"
+        blackRgb="42, 40, 51"
+      />
+      <HeaderTwo headerStyle="mainmenu-seven" header={7} />
+      <MobileMenu />
+      <SearchPopup />
+      <SliderSeven />
+      <Text/>
+      <ServiceSeven />
+      <AboutNine />
+      <AboutEight />
+      <ProjectSeven />
+      <WeDOSection/>
+      <AboutSection/>
+      <PricingTwo />
+      <FunFactSeven />
+      <SponsorSeven />
+      <TestimonialsSection />
+      <BlogSeven />
+      <SubscribeTwo />
+      <FooterSeven />
+    </Layout>
   );
 };
 
 export default Home7;
-
-
-
-
-
-
